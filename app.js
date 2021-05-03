6dcd5704ce3f51a3fc050acd392992bf8c885677
Lucas Meier <meiluca@dunwoody.edu>
2021-05-03 16:29:54 -0500

    asdf

--- app.js
+++ app.js
@@ -5,7 +5,10 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var homeRouter = require('./routes/home');
+var aboutRouter = require('./routes/about');
+var contactRouter = require('./routes/contact');
+var logonRouter = require('./routes/log-on');
 
 var app = express();
 
@@ -20,7 +23,10 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/home', homeRouter);
+app.use('/about', aboutRouter);
+app.use('/contact', contactRouter);
+app.use('/logon', logonRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/log-on.js
+++ routes/log-on.js
@@ -2,8 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/log-on', function(req, res, next) {
+  res.sendFile('./views/Log-On.html');
 });
 
 module.exports = router;
